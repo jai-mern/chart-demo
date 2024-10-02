0fd2ae44023fc6dfa0fce23cceca7c4a721f5421
jai-mern <Kjaya8862@gamail.com>
2024-10-02 07:30:43 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LineChart from './components/LineChart';
+import BarChart from './components/BarChart';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chart.js Graphs in React</h1>
+      <div style={{ width: '600px', margin: '0 auto' }}>
+        <h2>Line Chart</h2>
+        <LineChart />
+        <h2>Bar Chart</h2>
+        <BarChart />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
